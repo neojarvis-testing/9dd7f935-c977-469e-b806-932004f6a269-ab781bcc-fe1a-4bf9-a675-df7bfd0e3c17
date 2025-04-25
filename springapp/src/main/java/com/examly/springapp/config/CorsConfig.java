79872fc48838e1acb2515bf305d8aefe78612f44
Nihal-Joshi <nihaljoshinj21@gmail.com>
2025-04-25 16:32:40 +0000

    Security completed

--- springapp/src/main/java/com/examly/springapp/config/CorsConfig.java
+++ springapp/src/main/java/com/examly/springapp/config/CorsConfig.java
@@ -1,5 +1,16 @@
 package com.examly.springapp.config;
 
-public class CorsConfig {
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+            .allowedOrigins("*")
+            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+            .allowedHeaders("*")
+            .allowCredentials(false);
+    }
 }
